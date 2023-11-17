988e7393b02b1e867e8f2e540bf0c1f6e59ad984
black2 <rojas.ignacio.a.1@gmail.com>
2023-11-17 17:10:13 -0300

    commit charge requeriments.txt prueba base dir build

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'dist')],
+        "DIRS": [os.path.join(BASE_DIR, 'build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
